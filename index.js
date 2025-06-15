33e549346c424e7baa477c258f71fbfa95587e9a
Yash <yashsainani63@gmail.com>
2025-06-15 19:54:53 +0530

    Initial commit

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ app.use("/api/v1/user",userRouter);
 app.use("/api/v1/admin",userRouter);
 app.use("/api/v1/course", courseRouter); 
 
+app.use("/api/v2/user",userRouter);
+app.use("/api/v2/admin",userRouter);
+app.use("/api/v2/course", courseRouter); 
+
+async function main(){
+  await mongoose.connect("mongodb+srv://Yashsainani123:Yashsainani@1234@yashsainani123.kcvlkn5.mongodb.net/CourseEra-App")
+  app.listen(3000)
+    console.log("listening on port 3000");
+  }
 app.listen(3000);
